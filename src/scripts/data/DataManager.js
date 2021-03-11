ddf010afe99c10f6adfbbb4f12efe3bbdd2eda32
NSPatton <greengun15@gmail.com>
2021-03-11 13:28:22 -0600

    got posts to work

--- src/scripts/data/DataManager.js
+++ src/scripts/data/DataManager.js
@@ -8,15 +8,33 @@ export const getUsers = () => {
     // })
 }
 
+export const createPost = postObj => {
+  return fetch("http://localhost:8088/posts", {
+      method: "POST",
+      headers: {
+          "Content-Type": "application/json"
+      },
+      body: JSON.stringify(postObj)
 
+  })
+      .then(response => response.json())
+}
+
+let postCollection = [];
+
+export const usePostCollection = () => {
+  //Best practice: we don't want to alter the original state, so
+  //make a copy of it and then return it
+  //The spread operator makes this quick work
+  return [...postCollection];
+}
 export const getPosts = () => {
-
-    return fetch("http://localhost:8088/posts")
+  return fetch("http://localhost:8088/posts")
     .then(response => response.json())
-    // .then(parsedResponse => {
-    //     // do something with response here
-    //     return parsedResponse;
-    // })
+    .then(parsedResponse => {
+      postCollection = parsedResponse
+      return parsedResponse;
+    })
 }
 
 const loggedInUser = {
